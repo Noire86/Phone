8212252206d17ae3c4c81e3234adecec356417ec
SoularX86 <gleb.iwanow2010@yandex.ru>
2021-12-19 22:36:18 +0500

    Bruh

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Phone phone = new Phone("9695999", "MTS");
 
+        phone.printMenu();
+
 
 
     }
--- src/Phone.java
+++ src/Phone.java
@@ -4,6 +4,7 @@ import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Phone {
     private String myNumber;
@@ -68,13 +69,82 @@ public class Phone {
         return "Contact " + name + " was not found!";
     }
 
-    public void printAll(){
+    public void printAll() {
         System.out.println("*********************************");
-        for(int i = 0; i < contacts.size(); i++){
+        for (int i = 0; i < contacts.size(); i++) {
 
             System.out.printf("Contact #%d Name: %s Number: %s%n", i + 1, contacts.get(i).getName(), contacts.get(i).getPhoneNumber());
         }
         System.out.println("*********************************");
+    }
+
+    public void printMenu() {
+        Scanner scanner = new Scanner(System.in);
+        boolean isMenuActive = true;
+
+
+        while (isMenuActive) {
+            System.out.println("This is the main menu. Please provide the number of the needed menu section:\n" +
+                    "1. Print all contacts\n" +
+                    "2. Add new contact\n" +
+                    "3. Update existing contact\n" +
+                    "4. Remove contact\n" +
+                    "5. Quit menu");
+
+            int input = scanner.nextInt();
+
+
+            switch (input) {
+                case 1:
+                    System.out.println("Printing all contacts:");
+                    printAll();
+                    break;
+                case 2:
+                    System.out.println("Please input the contact name:");
+                    String name = scanner.next();
+                    System.out.println("Please input the phone number:");
+                    String phoneNumber = scanner.next();
+                    addContact(name, phoneNumber);
+                    break;
+                case 3:
+                    System.out.println("Please select the contact you wish to edit:");
+                    String contactName = scanner.next();
+                    if (!ifExists(contactName)) {
+                        System.out.println("This contact does not exist, try again");
+                        break;
+                    }
+                    System.out.println("What option do you want to edit? 1. Name 2. Phone number");
+                    int var = scanner.nextInt();
+
+                    switch (var) {
+                        case 1:
+                            System.out.println("Please enter a new name for the contact:");
+                            String newName = scanner.next();
+                            editName(contactName, newName);
+                            break;
+                        case 2:
+                            System.out.println("Please enter a new number for the contact:");
+                            String newNumber = scanner.next();
+                            editNumber(contactName, newNumber);
+                            break;
+                        default:
+                            System.out.println("Please enter a value 1 or 2");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Please enter the contact name you wish to remove:");
+                    String removingContact = scanner.next();
+                    removeContact(removingContact);
+                    break;
+                case 5:
+                    System.out.println("Exiting menu...");
+                    isMenuActive = false;
+                    break;
+                default:
+                    System.out.println("Please enter the correct menu value!");
+            }
+        }
+
     }
 
     private Contact getContactByName(String name) {
